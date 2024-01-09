c130706fb5131804bcc02ad9df4085d2fca93937
Adrian Antkowiak <adiantek@gmail.com>
2024-01-09 14:21:23 +0100

    Vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+document.documentElement.setAttribute('data-bs-theme', 'dark');
+
+</script>
+<style scoped>
+.loader {
+  background-color: var(--bs-body-bg);
+  z-index: 10;
+}
+</style>
+<template>
+  <div class="h-100 w-100 d-flex flex-column align-items-center justify-content-center loader">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden"></span>
+    </div>
+    <div>
+      Tu będzie projekt
+    </div>
+  </div>
+  <BToaster />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/main.scss';
+
+import { createApp } from 'vue';
+
+import App from './App.vue';
+
+const app = createApp(App)
+
+app.mount('#app')
