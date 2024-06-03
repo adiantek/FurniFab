ffb8de2db209b81a2bd85209e47975084c8ea275
Adrian Antkowiak <adiantek@gmail.com>
2024-06-03 12:21:37 +0200

    remove unused I/O from python & rust

--- src-tauri/src/max_flow_min_cost.rs
+++ src-tauri/src/max_flow_min_cost.rs
@@ -23,33 +23,21 @@ type RawOutput = (HashMap<String, HashMap<String, [u64; 3]>>, u64, u64);
 
 #[tauri::command]
 pub async fn run_max_flow_min_cost(
-    handle: AppHandle,
     edges: Vec<Vec<Edge>>,
 ) -> Result<(Vec<Vec<Edge>>, u64, u64), Error> {
-    let path = handle
-        .path_resolver()
-        .app_local_data_dir()
-        .and_then(|mut path| {
-            std::fs::create_dir_all(&path).ok().map(|_| {
-                path.push(FILENAME);
-                path
-            })
-        })
-        .ok_or(Error::NoPath)?;
 
-    let path_str = path.to_str().ok_or(Error::NoPath)?;
-
-    let mut file = std::fs::File::create(&path)?;
+    let mut lines: Vec<String> = Vec::new();
 
     for (index, edges) in edges.into_iter().enumerate() {
-        file.write_all(format!("{}:", index + 1).as_bytes())?;
+        let mut line = String::new();
+        line.push_str(format!("{}:", index + 1).as_str());
         for edge in edges {
-            file.write_all(format!(" {}:{},{}", edge.to, edge.capacity, edge.cost).as_bytes())?;
+            line.push_str(format!(" {}:{},{}", edge.to, edge.capacity, edge.cost).as_str());
         }
-        file.write_all(b"\n")?;
+        lines.push(line);
     }
 
-    let (graph, flow, cost): RawOutput = Python::eval_with_gil(SCRIPT, "start", path_str)?;
+    let (graph, flow, cost): RawOutput = Python::eval_with_gil(SCRIPT, "start", lines)?;
 
     let mut result = Vec::with_capacity(graph.len());
 
